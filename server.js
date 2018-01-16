9ba6167d7a2e15c04e806abfd1490f0cb7125bed
Julian Madle <julian@splash.devserver.biz>
2018-01-16 12:44:05 +0000

    added Application Insights support

--- server.js
+++ server.js
@@ -4,4 +4,7 @@
  * Module dependencies.
  */
 var app = require('./config/lib/app');
+const appInsights = require("applicationinsights"); 
+appInsights.setup("<instrumentation_key>"); 
+appInsights.start();
 var server = app.start();
